9de52c77b01848d714bccd8b258137466f3d4987
noxan <noxan@byteweaver.net>
2013-06-23 20:16:23 +0200

    use path.join instead of plus operator to join paths

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(express.favicon());
