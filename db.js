64b0191fa9c426251dfe9f9c4bca0f160e276613
noxan <noxan@byteweaver.net>
2013-06-23 20:59:04 +0200

    remove database connection success debug

--- db.js
+++ db.js
@@ -5,8 +5,5 @@ mongoose.connect('mongodb://localhost/matchsystem');
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-  console.log('database connection succesfull!');
-});
 
 exports.db = db;
