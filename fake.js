963e4d4be4825b17fdee804a074539485ce45422
noxan <noxan@byteweaver.net>
2013-06-23 20:07:44 +0200

    add simple helper script with own db connection to fake some user data

--- fake.js
+++ fake.js
@@ -1,2 +1,21 @@
-var db = require('./db');
+var Faker = require('Faker');
+var models = require('./models');
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/matchsystem');
+var db = mongoose.connection;
+
+db.once('open', function callback () {
+    for(var i = 0; i < 20; i++) {
+        var user = new models.User({
+            username: Faker.Name.findName(),
+            password: '123',
+            email: Faker.Internet.email(),
+            registered: true
+        });
+
+        user.save();
+    }
+
+    db.close();
+});
