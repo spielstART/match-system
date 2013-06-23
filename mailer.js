8c2fb15bbfa176f5b2a0b44c1931026834b61789
noxan <noxan@byteweaver.net>
2013-06-23 19:32:07 +0200

    fix indention in mailer.js

--- mailer.js
+++ mailer.js
@@ -2,29 +2,29 @@ var nodemailer = require("nodemailer");
 
 
 function sendMail (usermail, mailbody) {
+  var smtpTransport = nodemailer.createTransport('SMTP', {
+    service: 'Gmail',
+    auth: {
+      user: 'matchbracket@gmail.com',
+      pass: 'bracketmatch'
+    }
+  });
 
-	var smtpTransport = nodemailer.createTransport('SMTP', {
-		service: 'Gmail',
-		auth: {
-						user: 'matchbracket@gmail.com',
-						pass: 'bracketmatch'
-					}
-	});
+  var mailOptions = {
+    from: 'MatchSystem <matchbracket@gmail.com>',
+    to: usermail,
+    subject: 'Your registration on MatchSystem',
+    text:	mailbody,
+    html: mailbody
+  };
 
-	var mailOptions = {
-		from: 'MatchSystem <matchbracket@gmail.com>',
-		to: usermail,
-		subject: 'register@MatchSystem',
-		text:	mailbody,
-		html: mailbody
-	};
-
-	smtpTransport.sendMail(mailOptions, function(err, res){
-		if (err)
-			console.log(err);
-		else
-			console.log("Message sent: " + res.message);
-	});
+  smtpTransport.sendMail(mailOptions, function(err, res) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Message sent: " + res.message);
+    }
+  });
 }
 
 exports.sendMail = sendMail;
