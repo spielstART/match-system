4b47f71f8fc92e0ec681b9977191399f0428716c
noxan <noxan@byteweaver.net>
2013-06-23 20:59:39 +0200

    fix fake script and use db module for database connection

--- fake.js
+++ fake.js
@@ -1,21 +1,19 @@
 var Faker = require('Faker');
 var models = require('./models');
+var db = require('./db');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/matchsystem');
-var db = mongoose.connection;
 
-db.once('open', function callback () {
-    for(var i = 0; i < 20; i++) {
-        var user = new models.User({
-            username: Faker.Name.findName(),
-            password: '123',
-            email: Faker.Internet.email(),
-            registered: true
-        });
+for(var i = 0; i < 20; i++) {
+    var user = new models.User({
+        username: Faker.Name.findName(),
+        password: '123',
+        email: Faker.Internet.email(),
+        registered: true
+    });
 
-        user.save();
-    }
+    user.save();
+}
 
-    db.close();
-});
+
+db.db.close();
